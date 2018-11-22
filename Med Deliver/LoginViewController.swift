0f5fea75c7ce728ebfce21f5a247cbf4f89462cb
supath <supath@live.co.uk>
2018-11-22 12:48:06 +0000

    1st commit

---
+++ Med Deliver/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  Med Deliver
+//
+//  Created by Supath Shrestha on 19/11/2018.
+//  Copyright © 2018 Supath Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
