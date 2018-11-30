ae6105ef9ed2dd9aee1032edb0156b46dc572cf4
supath <supath@live.co.uk>
2018-11-30 12:36:15 +0000

    login

---
+++ Med Deliver/UiViewGradient.swift
@@ -0,0 +1,26 @@
+//
+//  UiViewGradient.swift
+//  Med Deliver
+//
+//  Created by Supath Shrestha on 17/10/2018.
+//  Copyright © 2018 Supath Shrestha. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
